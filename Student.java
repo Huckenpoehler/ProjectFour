1340f809bdea0316ea1966be5c183365a9770bd3
pravalcodes <97808881+pravalcodes@users.noreply.github.com>
2022-04-01 12:11:08 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,14 @@
+public class Student {
+    private int submissions;
+    private String userName;
+    private String quizName;
+
+    public Student(int submissions, String userName, String quizName) {
+        this.submissions = submissions;
+        this.userName = userName;
+        this.quizName = quizName;
+    }
+
+    public static 
+}
 
